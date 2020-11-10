e9959cd6b999d396d0016d5940b802ee70307e54
Jonathan Arias <jony1754@hotmail.com>
2020-11-10 17:56:06 -0500

    Updates

---
+++ precache-manifest.f2f5821d4217811a9f2f679e4268ee42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ce814e161425d7613e45fcbd63aaba",
+    "url": "/climApp/index.html"
+  },
+  {
+    "revision": "1a207fc847dadc9e9beb",
+    "url": "/climApp/static/css/main.c2262d88.chunk.css"
+  },
+  {
+    "revision": "8051c1c485a4a64ac0e6",
+    "url": "/climApp/static/js/2.d3d74ad1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/climApp/static/js/2.d3d74ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a207fc847dadc9e9beb",
+    "url": "/climApp/static/js/main.830ee211.chunk.js"
+  },
+  {
+    "revision": "93492e5fd67332313c75",
+    "url": "/climApp/static/js/runtime-main.1d4c12ef.js"
+  },
+  {
+    "revision": "e4f16078704a9b7f3dd474b62a53c9fe",
+    "url": "/climApp/static/media/thunder.e4f16078.jpg"
+  }
+]);
